2b1a26c2f698d4c699b4bb8b95c8a1f1eaa2d222
Nate Osterfeld <osterfnl@mail.uc.edu>
2026-03-01 22:29:05 -0500

    Add User entity and load to db

---
+++ backend/src/main/java/com/bookstore/users/User.java
@@ -0,0 +1,47 @@
+package com.bookstore.users;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String passwordHash;
+
+    public User(Long id, String email, String passwordHash) {
+        this.id = id;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public User() {
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
