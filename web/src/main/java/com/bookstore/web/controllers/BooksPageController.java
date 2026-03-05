c9e77d7b7ea3ecb5756bca8372ed7ad3a7ec4908
Nate Osterfeld <osterfnl@mail.uc.edu>
2026-03-04 19:45:13 -0500

    Books (web) proof of concept

---
+++ web/src/main/java/com/bookstore/web/controllers/BooksPageController.java
@@ -0,0 +1,27 @@
+package com.bookstore.web.controllers;
+
+import com.bookstore.web.clients.IBooksClient;
+import com.bookstore.web.external.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class BooksPageController {
+    IBooksClient booksClient;
+
+    public BooksPageController(IBooksClient booksClient) {
+        this.booksClient = booksClient;
+    }
+
+    @GetMapping
+    public String getBooks(Model model) {
+        List<Book> books = booksClient.getBooks();
+        model.addAttribute("books", books);
+        return "books";
+    }
+}
---
+++ web/src/main/java/com/bookstore/web/external/Book.java
@@ -0,0 +1,101 @@
+package com.bookstore.web.external;
+
+import java.math.BigDecimal;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String description;
+    private String genre;
+    private BigDecimal price;
+    private Integer quantity;
+    private String coverImageUrl;
+    private Long authorId;
+    private String authorName;
+
+    public Book(Long id, String title, String description, String genre, BigDecimal price, Integer quantity, String coverImageUrl, Long authorId, String authorName) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.genre = genre;
+        this.price = price;
+        this.quantity = quantity;
+        this.coverImageUrl = coverImageUrl;
+        this.authorId = authorId;
+        this.authorName = authorName;
+    }
+
+    public Book() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCoverImageUrl() {
+        return coverImageUrl;
+    }
+
+    public void setCoverImageUrl(String coverImageUrl) {
+        this.coverImageUrl = coverImageUrl;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+}
