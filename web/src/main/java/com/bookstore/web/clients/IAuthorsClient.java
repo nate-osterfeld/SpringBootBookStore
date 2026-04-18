ed5dd624de640dc062d8a9576e4333fd13cfe3be
milesvcresci <milesvcresci@gmail.com>
2026-04-18 13:40:59 -0400

    Add public authors list page

--- web/src/main/java/com/bookstore/web/clients/IAuthorsClient.java
+++ web/src/main/java/com/bookstore/web/clients/IAuthorsClient.java
@@ -4,12 +4,16 @@ import com.bookstore.web.external.Author;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
 
 @FeignClient(
         name = "AUTHORS-PUBLIC-SERVICE",
         url = "http://localhost:5000/api/authors"
 )
 public interface IAuthorsClient {
+
+    @GetMapping
+    List<Author> getAuthors();
 
     @GetMapping("/{id}")
     Author getAuthorById(@PathVariable("id") Long id);
